5e548d35095772cc13c1159c297f8f8efe8390eb
Craig Stuntz <craigstuntz@gmail.com>
2015-05-18 23:01:50 -0400

    Simpler test example.

---
+++ FSharpNUnit.Program/Expression.fs
@@ -0,0 +1,9 @@
+﻿module Expression
+
+type Expression = 
+    | Constant of int
+    | Sum of Expression * Expression
+
+let rec value = function
+| Constant i -> i
+| Sum (left, right) -> (value left) + (value right)
---
+++ FSharpNUnit.Tests/ExpressionTests.fs
@@ -0,0 +1,9 @@
+﻿module ExpressionTests
+
+open NUnit.Framework
+open FsUnit
+open Expression
+
+[<Test>]
+let ``Value of simple example`` () = 
+    value <| Sum (Constant 1, (Sum (Constant 2, Constant 3))) |> should equal 6
