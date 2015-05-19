04c2a22c1f439c04ba56de211a4637f10a6ad4ee
Craig Stuntz <craigstuntz@gmail.com>
2015-05-18 23:42:22 -0400

    Renamed files

--- FSharpNUnit.Tests/TreeTests.fs
+++
@@ -1,9 +0,0 @@
-﻿module TreeTests
-
-open NUnit.Framework
-open FsUnit
-open Tree
-
-[<Test>]
-let ``Count of simple tree`` () = 
-    Branch [ Branch [ Leaf 1; Leaf 2]; Branch [Leaf 3]] |> Tree.leafCount |> should equal 3
